3a3bb6cf7b0c36fe2b0d74bf397b31152448f25b
Eric Wendelin <me@eriwen.com>
2018-09-23 15:36:56 -0700

    Attempt to convert build scripts to Kotlin and pre-compiled script plugins

---
+++ buildSrc/src/main/kotlin/build-scan-config.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `build-scan`
+}
+
+//buildScan {
+//    setTermsOfServiceUrl("https://gradle.com/terms-of-service")
+//    setTermsOfServiceAgree("yes")
+//    if (!System.getenv("CI").isNullOrEmpty()) {
+//        publishAlways()
+//        tag("CI")
+//    }
+//}
